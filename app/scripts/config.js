0a294ea485f6dd01525c0c8a1d4c8df6d68b4c81
beam018 <beam018@gmail.com>
2012-12-24 20:14:45 +0400

    #1 #2

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,8 +2,8 @@ define([], function(){
   'use strict';
 
   var config = {
-    // serverUrl: 'http://localhost:3501/'
-    serverUrl: 'http://at.svetleo.com/'
+    serverUrl: 'http://localhost:3501/'
+    // serverUrl: 'http://at.svetleo.com/'
   };
 
   return config;
