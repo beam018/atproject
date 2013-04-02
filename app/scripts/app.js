d335dc138628eae64a24b2dfac2a7308ea147ec3
beam018 <beam018@gmail.com>
2013-04-02 22:37:59 +0400

    scroll fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,8 +27,7 @@ define([
     var body = document.getElementsByTagName('body')[0];
     var $body = $(body);
     var leftOffset = body.getBoundingClientRect().left;
-    console.log(leftOffset);
-    // $body.css('margin-left', leftOffset + 'px');
+    $body.css('margin-left', leftOffset + 'px');
 
     $(window).resize(function(e){
       $body.css('margin-left', 'auto');
